caa1b04797681c09163600af22076f904bfc424f
MANH-IT\manh0 <manh0974@gmail.com>
2021-12-24 01:42:02 +0700

    commit1

--- src/index.js
+++ src/index.js
@@ -72,6 +72,6 @@ app.set('views', path.join(__dirname, 'resources/views'));
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-  // console.log(`Listening on port ${port}`);
+  // console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Listening on port ${port}`);
 });
